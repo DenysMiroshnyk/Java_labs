0ebae6a5a193deddc70d466439ba2a46d5b83fdf
DenysMiroshnyk <Varhan784512@gmail.com>
2018-06-01 17:44:43 +0300

    Adding Lab #3

---
+++ Lab_3/src/main/java/com/denys/Main.java
@@ -0,0 +1,27 @@
+package com.denys;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int replaceFrom = 0;
+        int replaceTo = 0;
+        String replacer = "REPLACED";
+        StringBuffer buff = new StringBuffer("One design goal of Java is portability, which means that programs written for the Java platform must run similarly on any combination of hardware and operating system with adequate runtime support. This is achieved by compiling the Java language code to an intermediate representation called Java bytecode, instead of directly to architecture-specific machine code. Java bytecode instructions are analogous to machine code, but they are intended to be executed by a virtual machine (VM) written specifically for the host hardware. End users commonly use a Java Runtime Environment (JRE) installed on their own machine for standalone Java applications, or in a web browser for Java applets.");
+
+        System.out.println("Our string is: " + buff.toString());
+        for (int index = 0; index < buff.length(); index++){
+            if (buff.charAt(index) == ' '){
+                replaceTo = index;
+            }
+            if(replaceTo - replaceFrom == replacer.length()){
+                buff.replace(replaceFrom, replaceTo, replacer);
+                replaceFrom = replaceTo + 1;
+            }
+            else{
+                replaceFrom = replaceTo + 1;
+            }
+        }
+
+        System.out.println("Our string now is: " + buff.toString());
+    }
+}
